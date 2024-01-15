9eccdaf255fe458255037443319c74511bcdca6d
zhe yuan <zheyuan@zhes-air.lan>
2024-01-15 14:02:02 -0800

    testing connection

--- addTwoNumber.py
+++ addTwoNumber.py
@@ -3,6 +3,7 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+        self.use = None
 
 class Solution:
     def addTwoNumbers(self, l1: [ListNode], l2: [ListNode]) -> [ListNode]:
